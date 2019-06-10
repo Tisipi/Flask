8110392aafc278be573baf58fd1b08a2d2d7c751
Tisipi <Tisipi@tisipi.com>
2019-06-10 21:05:51 +0200

    User agent test

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import request
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h2>Hello, World!</h2>'
+    # return '<h2>Hello, World!</h2>'
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
 
 
 @app.route('/user/<name>')
