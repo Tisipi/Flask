8805664c54da31f61b68898505381aea659f44ee
Tisipi <Tisipi@tisipi.com>
2019-06-14 17:43:55 +0200

    Rendering Jinja template

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask import render_template
+
 from flask import request
+from flask import redirect
 
 
 app = Flask(__name__)
@@ -7,11 +10,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return '<h2>Hello, World!</h2>'
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent)
-
+    # user_agent = request.headers.get('User-Agent')
+    # return '<p>Your browser is {}. Headers are: {} </p>'.format(user_agent, request.headers)
+    # return redirect('http://www.docker.com')
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h2>Hello, dear {}!</h2>'.format(name)
+    # return '<h2>Hello, dear {}!</h2>'.format(name)
+    return render_template('user.html', name=name)
+
